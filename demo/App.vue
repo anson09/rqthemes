f42fce3302c08d820a5fa14d00b321ad5cd0c697
Arius <arius@quietboy.local>
2019-09-20 14:31:00 +0800

    fix: ie error

--- demo/App.vue
+++ demo/App.vue
@@ -227,6 +227,22 @@
             </el-table-column>
           </el-table>
         </el-row>
+        <el-row>
+          <el-table
+            :data="tableData1"
+            style="width: 600px;margin-top: 20px;"
+            row-key="id"
+            border
+            default-expand-all
+            :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
+          >
+            <el-table-column prop="date" label="日期" sortable width="180">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" sortable width="180">
+            </el-table-column>
+            <el-table-column prop="address" label="地址"> </el-table-column>
+          </el-table>
+        </el-row>
         <h4>Tag</h4>
         <el-row>
           <el-tag class="demo-item" closable>Tag One</el-tag>
@@ -756,6 +772,74 @@ export default {
           city: "普陀区",
           address: "上海市普陀区金沙江路 1516 弄",
           zip: 200333
+        }
+      ],
+      tableData1: [
+        {
+          id: 1,
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          id: 2,
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          id: 3,
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+          children: [
+            {
+              id: 31,
+              date: "2016-05-01",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1519 弄"
+            },
+            {
+              id: 32,
+              date: "2016-05-01",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1519 弄",
+              children: [
+                {
+                  id: 321,
+                  date: "2016-05-01",
+                  name: "王小虎",
+                  address: "上海市普陀区金沙江路 1519 弄",
+                  children: [
+                    {
+                      id: 3211,
+                      date: "2016-05-01",
+                      name: "王小虎",
+                      address: "上海市普陀区金沙江路 1519 弄"
+                    },
+                    {
+                      id: 3212,
+                      date: "2016-05-01",
+                      name: "王小虎",
+                      address: "上海市普陀区金沙江路 1519 弄"
+                    }
+                  ]
+                },
+                {
+                  id: 322,
+                  date: "2016-05-01",
+                  name: "王小虎",
+                  address: "上海市普陀区金沙江路 1519 弄"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 4,
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
         }
       ],
       menu: "1",
