a55a8eb116447345ee74320701ddfc07b09c6690
anson <anson@ricequant.com>
2020-09-08 15:10:03 +0800

    feat: build validating

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu
+# set -euo pipefail
 
 parcel() {
     SOURCE=$1
@@ -9,8 +9,33 @@ parcel() {
 
 ./scripts/updep.sh
 rm -rf lib
+
 parcel src/index.scss index.css
 LEGACY=true parcel src/index.scss legacy.css
 parcel src/element-ui/light.scss element-light.css
 parcel src/element-ui/product.scss element-product.css
 parcel src/element-ui/dark.scss element-dark.css
+
+INJECT_CONTENT=node_modules/element-ui/lib/theme-chalk/base.css
+LEGACY_FILE_PATH=lib/legacy.css
+TARGETS=(lib/index.css $LEGACY_FILE_PATH)
+
+for ITEM in ${TARGETS[@]}
+do
+    awk '{print}' $INJECT_CONTENT $ITEM > $ITEM.tmp
+    mv $ITEM.tmp $ITEM
+done
+
+cp -R node_modules/element-ui/lib/theme-chalk/fonts lib
+
+UNCOEVR_VARIABLES=$(cat $LEGACY_FILE_PATH|grep -A 1 undefined)
+
+if [ -n "$UNCOEVR_VARIABLES" ]
+then
+    echo ">>> illegal legacy.css"
+    echo $UNCOEVR_VARIABLES
+    rm -rf $LEGACY_FILE_PATH
+    exit 1
+fi
+
+echo "$(sed '/:root{}/d' $LEGACY_FILE_PATH)" > $LEGACY_FILE_PATH
