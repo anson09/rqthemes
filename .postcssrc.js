c8f826bf2e22c29762b07e9c8b78b357517d9030
anson <anson@ricequant.com>
2020-09-08 18:38:42 +0800

    revert: fetch vars logic

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,34 +1,26 @@
 const fs = require("fs");
 const path = require("path");
 
-let json = null;
 function getCssVar() {
-  if (json) {
-    return json;
-  } else {
-    once = true;
-    const FILES = ["global", "vars/light", "vars/compatibility"];
+  const FILES = ["global", "vars/light", "vars/compatibility"];
 
-    let source = FILES.map((file) =>
-      fs.readFileSync(path.resolve(__dirname, `src/${file}.scss`), "utf8")
-    ).join("\n");
+  let source = FILES.map((file) =>
+    fs.readFileSync(path.resolve(__dirname, `src/${file}.scss`), "utf8")
+  ).join("\n");
 
-    let trans = source
-      .match(/--.*:[\s\S]*?;/g)
-      .join()
-      .replace(/(\s)/g, "")
-      .replace(/--(.*?):(.*?);/g, function (match, p1, p2) {
-        return `"--${p1}":"${p2}"`;
-      });
-
-    jsonStr = `{${trans}}`;
-    fs.writeFileSync("./lib/light.json", jsonStr, () => {
-      console.log("<collecting vars to light.json success>");
+  let trans = source
+    .match(/--.*:[\s\S]*?;/g)
+    .join()
+    .replace(/(\s)/g, "")
+    .replace(/--(.*?):(.*?);/g, function (match, p1, p2) {
+      return `"--${p1}":"${p2}"`;
     });
 
-    json = JSON.parse(jsonStr);
-  }
-  return json;
+  const json = `{${trans}}`;
+
+  fs.writeFileSync("./lib/light.json", json);
+
+  return JSON.parse(json);
 }
 
 module.exports = {
--- TODO.md
+++
@@ -1,2 +0,0 @@
-1. light.json 输出脚本和校验命令
-2. 发布 next 版本，一个产品试用，break change announcement
