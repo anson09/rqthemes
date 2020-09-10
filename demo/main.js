bd961d0a8b62c325da1ef5d1eb98d51d37afcec5
anson <anson@ricequant.com>
2020-09-10 10:01:21 +0800

    feat: dev mode instant review

--- demo/main.js
+++ demo/main.js
@@ -1,10 +1,19 @@
 import Vue from "vue";
 import elementUI from "element-ui";
 import App from "./App.vue";
-import "../lib/element-light.css";
-import "../lib/element-dark.css";
-import "../lib/element-product.css";
-import "../lib/index.css";
+
+/* build from source */
+import "element-ui/lib/theme-chalk/base.css";
+import "../src/element-ui/light.scss";
+import "../src/element-ui/dark.scss";
+import "../src/element-ui/product.scss";
+import "../src/index.scss";
+
+/* build from lib */
+// import "../lib/element-light.css";
+// import "../lib/element-dark.css";
+// import "../lib/element-product.css";
+// import "../lib/index.css";
 
 Vue.use(elementUI);
 
