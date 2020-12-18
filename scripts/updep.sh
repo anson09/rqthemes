0612a8bdaf375c4a1e9394bcbad744a4efd3cb16
anson <anson@ricequant.com>
2020-12-18 10:12:05 +0800

    ci: updep version robust

--- scripts/updep.sh
+++ scripts/updep.sh
@@ -3,17 +3,29 @@ set -eu
 
 LAST_VERSION=$(<depVersion)
 
+if [[ ${#LAST_VERSION} != 8 && ${#LAST_VERSION} != 10 ]]; then
+  echo "depVerison format: YYYYMMDD or YYYYMMDDTT(TT stand for 00 01...)"
+  exit 1
+fi
+
 if [ ! -f .depVersion ]; then
   touch .depVersion
-  echo $LAST_VERSION > .depVersion
+  echo $LAST_VERSION >.depVersion
   npm ci
   exit 0
 fi
 
 LOCAL_VERSION=$(<.depVersion)
 
-if [ $LAST_VERSION -gt $LOCAL_VERSION ]; then
-  echo $LAST_VERSION > .depVersion
+if [[ $LAST_VERSION < $LOCAL_VERSION ]]; then
+  echo -e "broken version:\nlocal verion - $LOCAL_VERSION\nlast version - $LAST_VERSION\n\
+please ensure last version accurately,and run command again"
+  rm .depVersion
+  exit 1
+fi
+
+if [[ $LAST_VERSION > $LOCAL_VERSION ]]; then
+  echo $LAST_VERSION >.depVersion
   npm ci
   exit 0
 fi
